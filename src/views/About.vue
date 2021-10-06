5c1fe80d763aa2969a1e2b3bc3b73f272327a38e
CodingBee77 <00martyna.znajdek00@gmail.com>
2021-10-06 19:47:41 +0200

    Add todo app

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-8">
+    <h1>About</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo autem,
+      consequuntur fugit rerum eaque beatae est dolores distinctio omnis nulla
+      pariatur obcaecati mollitia esse, alias accusamus ullam temporibus
+      dignissimos commodi.
+    </p>
   </div>
 </template>
